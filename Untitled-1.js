dec7b12ca8fb28521e99beb138a4c2fbd1960a36
Johan Cruz <johan.cruz@tecsup.edu.pe>
2023-04-19 21:19:52 -0500

    Laboratorio finalizado

---
+++ Untitled-1.js
@@ -0,0 +1,15 @@
+var nombre="carlos";
+console.log(nombre);
+
+var numero=1000;
+console.log(numero);
+
+var numero=1000;
+console.log(numero);
+
+var objeto ={
+    nombre:"juan",
+    apellido:"perez",
+    telefono:"55443322"
+}
+console.log(objeto);
---
+++ actividad6-1.js
@@ -0,0 +1,10 @@
+let nombreCompleto = "juan perez";
+let nombrecompleto = "carlos lara";
+console.log(nombreCompleto);
+console.log(nombrecompleto);
+
+let alnombreCompleto;
+let _nombreCompleto;
+let $nombreCompleto;
+
+let ruptura =10;
